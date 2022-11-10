77b34745eb4afd7e1eb9455a314f37ada865acc9
neschkonesic <114104457+neschkonesic@users.noreply.github.com>
2022-11-10 20:22:36 +0100

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Helmet from 'react-helmet';
-
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
@@ -9,20 +7,6 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-
-        <Helmet>
-            <!-- Begin of Chaport Live Chat code -->
-<script type="text/javascript">
-(function(w,d,v3){
-w.chaportConfig = {
-  appId : '636a562520f872d7d1c6bada'
-};
-
-if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
-</script>
-<!-- End of Chaport Live Chat code -->
-         </Helmet>
-
          <div
           className="center margin-top-2"
           style={{
